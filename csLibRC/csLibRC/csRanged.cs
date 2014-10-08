29c6778aba871ffe324fc5bd7e3a69593bd8cf31
kerenskylegacy <kerensky.legacy@gmail.com>
2014-10-09 00:05:00 +0200

    csRanged
    Klasse um Objekte für Fernwaffen zu erzeugen

---
+++ csLibRC/csLibRC/csRanged.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioactiveConversions
+{
+    class csRanged : csItems
+    {
+        private string _cal;
+
+        public string cal
+        {
+            get { return _cal; }
+            set { _cal = value; }
+        }
+
+        private string _group;
+
+        public string group
+        {
+            get { return _group; }
+            set { _group = value; }
+        }
+
+        private string _modus;
+
+        public string modus
+        {
+            get { return _modus; }
+            set { _modus = value; }
+        }
+
+        private int _rc;
+
+        public int rc
+        {
+            get { return _rc; }
+            set { _rc = value; }
+        }
+
+        private int _magsize;
+
+        public int magsize
+        {
+            get { return _magsize; }
+            set { _magsize = value; }
+        }
+
+        public List<string> _mods = new List<string>();
+
+        private int _modCap;
+
+        public int modCap
+        {
+            get { return _modCap; }
+            set { _modCap = value; }
+        }
+        
+        
+        
+        
+        
+        
+    }
+}
